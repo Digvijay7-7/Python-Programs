eea7e4ec9bf789e0e2fbe62d6dc371fa0c505768
Digvijay Patil <digwijaypatil6521@gmail.com>
2023-10-02 23:12:29 +0530

    commit

---
+++ 46_list.py
@@ -0,0 +1,13 @@
+'''
+Create a dictionary from the following pairs (key, value):
+'USA': 'Washington'
+'Germany': 'Berlin'
+'Austria': 'Vienna'
+and print it to the console.
+
+Expected result:
+{'USA': 'Washington', 'Germany': 'Berlin', 'Austria': 'Vienna'}
+'''
+
+a = {'USA': 'Washington', 'Germany': 'Berlin', 'Austria': 'Vienna'}
+print(a)
