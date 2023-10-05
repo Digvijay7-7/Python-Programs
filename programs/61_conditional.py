0a7020e8efb87a53d2e8b95ccdda3dfcc19aff13
Digvijay Patil <digwijaypatil6521@gmail.com>
2023-10-05 12:59:11 +0530

    commit

---
+++ programs/61_conditional.py
@@ -0,0 +1,17 @@
+'''
+The following code is given:
+code = 'DSVNDOICSN'
+
+Check if the code variable has only uppercase letters. If so, print 'YES' to the console.
+
+Expected result:
+YES
+'''
+code = 'DSVNDOICSN'
+if code.upper():
+    print('YES')
+else:
+    print('NO')
+    
+# if code.isupper():
+#     print('YES')
