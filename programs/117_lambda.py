b776f35a6050a1f9f485fdfc2ccb652c89e14e68
Digvijay Patil <digwijaypatil6521@gmail.com>
2023-10-15 00:16:35 +0530

    changes

--- programs/117_lambda.py
+++ programs/117_lambda.py
@@ -11,3 +11,12 @@ Example:
 
 Note! You only need to define the function.
 '''
+
+def is_palindrome(string):
+    inverse = string[::-1]
+    if string == inverse:
+        return True
+    else:
+        return False
+    
+print(is_palindrome('level'))
